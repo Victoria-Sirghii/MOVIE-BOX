86764a5555925bddde0dc7535c46101c2030981a
Victoria <vika.sirghii@gmail.com>
2021-03-13 17:54:41 +0200

    display show result and small fetch ref

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,14 @@
-import React, {useState} from 'react'
-import MainPageLayout from '../components/MainPageLayout'
+import React, {useState} from 'react';
+import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
+  const [result, setResult ] = useState(null);
 
   const onSearch = () =>{
-    //http://api.tvmaze.com/search/shows?q=girls
-    fetch(`http://api.tvmaze.com/search/shows?q=${input}`)
-    .then(response => response.json())
-    .then(result => {
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResult(result)
       console.log(result)
     })
 }
@@ -22,10 +22,29 @@ const Home = () => {
     }
   }
 
+  const renderResults= () => {
+    if(result && result.length === 0){
+      return <div>No results</div>
+    }
+
+    if(result && result.length > 0){
+      return (
+      <div>
+        {result.map(item => (
+          <div key={item.show.id}>{item.show.name}</div>
+        ))}
+      </div>
+      )
+    }
+
+    return null;
+  }
+
   return (
     <MainPageLayout>
       <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
       <button type="button" onClick={onSearch}>Search</button>
+      {renderResults()}
     </MainPageLayout>
   )
 }
